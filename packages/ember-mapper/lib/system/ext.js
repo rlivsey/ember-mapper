ff44f589d102dba4fffa7b61ddb4ab462b68d5b9
Richard Livsey <richard@livsey.org>
2013-02-20 13:06:16 +0000

    remove unused vars from Route#model

--- packages/ember-mapper/lib/system/ext.js
+++ packages/ember-mapper/lib/system/ext.js
@@ -24,9 +24,7 @@ Ember.Route.reopen(StoreMixin, {
     if (!name && sawParams) { return params; }
     else if (!name) { return; }
 
-    var container = get(this, 'container');
-    var namespace = this.router.namespace;
-    var store     = this.storeFor(name);
+    var store = this.storeFor(name);
 
     // TODO - make sure we have a valid store & found a valid model class
 
